87196b801bf77ec5157686c9da5f10c7078a7d33
EhMigueh <miloaiza@umag.cl>
2025-04-11 00:17:37 -0300

    Se agregaron prints

--- srcdb/main.c
+++ srcdb/main.c
@@ -4,15 +4,20 @@ int main(void)
 {
     srand((unsigned int)time(NULL));
 
-    fprintf(stdout, "\nCREADO Bases de Datos...\n");
+    fprintf(stdout, "\nCREADO Bases de Datos...\n\n");
 
     generate_csv("./db/database10.csv", FIRST_DB_SIZE);
+    fprintf(stdout, "Base de Datos de 10000 objetos CREADA.\n");
     generate_csv("./db/database25.csv", SECOND_DB_SIZE);
+    fprintf(stdout, "Base de Datos de 25000 objetos CREADA.\n");
     generate_csv("./db/database50.csv", THIRD_DB_SIZE);
+    fprintf(stdout, "Base de Datos de 50000 objetos CREADA.\n");
     generate_csv("./db/database75.csv", FOURTH_DB_SIZE);
+    fprintf(stdout, "Base de Datos de 75000 objetos CREADA.\n");
     generate_csv("./db/database100.csv", FIFTH_DB_SIZE);
+    fprintf(stdout, "Base de Datos de 100000 objetos CREADA.\n");
 
-    fprintf(stdout, "Bases de Datos CREADAS exitosamente.\n\n");
+    fprintf(stdout, "\nBases de Datos CREADAS exitosamente.\n\n");
 
     return EXIT_SUCCESS;
 }
